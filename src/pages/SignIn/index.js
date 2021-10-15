cef0d500eae3635bfd8c86f1c5bb72326181abf1
Robson Gomes <robson.080490@gmail.com>
2021-10-15 10:50:45 -0300

    criando pagina login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,44 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.png';
+
+import './signin.css';
+
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert('TESTE')
+  }
+
   return (
-    <div>
-      <h1>Pagina Login!</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="logo do sistema"/>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input 
+            type="text" 
+            placeholder="digite seu email" 
+            value={email} onChange={(event) => setEmail(event.target.value)} 
+          />
+          <input 
+            type="password" 
+            placeholder="digite sua senha" 
+            value={password} 
+            onChange={(event) => setPassword(event.target.value)} 
+          />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
     </div>
   )
 }
