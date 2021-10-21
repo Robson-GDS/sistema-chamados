ef00225e423cbdfa31e1fefc66c8a3361c201fd0
Robson Gomes <robson.080490@gmail.com>
2021-10-21 09:17:59 -0300

    funcao de deslogar usuario

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,9 +3,12 @@ import { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth'
 
 export default function Dashboard() {
+  const { signOut } = useContext(AuthContext);
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={() => signOut()} >Fazer logout</button>
     </div>
   )
 }
