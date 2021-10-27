c4104b6f4ad68379c74c269086731b83078c8e47
Robson Gomes <robson.080490@gmail.com>
2021-10-27 09:15:54 -0300

    add Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,33 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
+
+import { AuthContext } from '../../contexts/auth';
+import avatar from '../../assets/avatar.png';
+
 import './header.css';
 
 export default function Header() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="sidebar">
+      <div>
+        <img src={user.avatarUrl === null ? avatar : user.avatarUrl } alt="foto avatar"/>
+      </div>
+
+      <Link to="/dashboard">
+        <FiHome color="#fff" size={24} />
+        Chamados
+      </Link>
+      <Link to="/dashboard">
+        <FiUser color="#fff" size={24} />
+        Clientes
+      </Link>
+      <Link to="/dashboard">
+        <FiSettings color="#fff" size={24} />
+        Configurações
+      </Link>
     </div>
   )
 }
