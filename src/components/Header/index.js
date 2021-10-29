054de60b600307945d3167c7cab8790840657f9d
Robson Gomes <robson.080490@gmail.com>
2021-10-29 08:25:04 -0300

    estilizando title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,11 +20,11 @@ export default function Header() {
         <FiHome color="#fff" size={24} />
         Chamados
       </Link>
-      <Link to="/dashboard">
+      <Link to="/customers">
         <FiUser color="#fff" size={24} />
         Clientes
       </Link>
-      <Link to="/dashboard">
+      <Link to="/profile">
         <FiSettings color="#fff" size={24} />
         Configurações
       </Link>
