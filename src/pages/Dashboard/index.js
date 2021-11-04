e3aee75375832310230f1cb1fba95060aad69ba6
Robson Gomes <robson.080490@gmail.com>
2021-11-04 08:45:59 -0300

    criando perfil

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -11,7 +11,6 @@ export default function Dashboard() {
       <Header />
 
       <h1>Dashboard</h1>
-      <button onClick={() => signOut()} >Fazer logout</button>
     </div>
   )
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,22 @@
-import { FiSettings } from 'react-icons/fi'
+import { useState, useContext } from 'react';
+import { FiSettings, FiUpload } from 'react-icons/fi'
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import avatar from '../../assets/avatar.png';
+
+import { AuthContext } from '../../contexts/auth';
 
 import './profile.css';
 
 export default function Profile() {
+  const { user, signOut } = useContext(AuthContext);
+
+  const [nome, setNome] = useState(user && user.nome);
+  const [email, setEmail] = useState(user && user.email);
+
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
   return(
     <div>
       <Header />
@@ -13,6 +24,37 @@ export default function Profile() {
         <Title name="Meu perfil">
           <FiSettings size={25} />
         </Title>
+
+        <div className="container">
+          <form className="form-profile">
+            <label className="label-avatar">
+              <span>
+                <FiUpload color="#fff" size={25} />
+              </span>
+
+              <input type="file" accept="image/*" /><br/>
+              { avatarUrl === null ? 
+                <img src={avatar} width="250" height="250" alt="Foto de perfil do usuario" />
+                :
+                <img src={avatarUrl} width="250" height="250" alt="Foto de perfil do usuario" />
+              } 
+            </label>
+
+            <label>Nome</label>
+            <input type="text" value={nome} onChange={(event) => setNome(event.target.value)} />
+
+            <label>Email</label>
+            <input type="text" value={email} disabled={true} />
+
+            <button type="submit">Salvar</button>
+          </form>
+        </div>
+
+        <div className="container">
+          <button className="logout-btn" onClick={() => signOut()}>
+            Sair
+          </button>
+        </div>
       </div>
     </div>
   );
